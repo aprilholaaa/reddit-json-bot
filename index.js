b8cbd6ba6299b9f18d146cf26c46f13aadd95666
aprilholaaa <aprilholaaa@gmail.com>
2026-05-07 10:36:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
-app.get("/convert", async (req, res) => {
-
-  try {
-
-    let link = req.query.link;
-
-    if (!link) {
-      return res.status(400).json({
-        error: "No link provided"
-      });
-    }
-
-    // remove query params
-    link = link.split("?")[0];
-
-    // remove ending slash
-    if (link.endsWith("/")) {
-      link = link.slice(0, -1);
-    }
-
-    // add .json
-    const jsonUrl = link + ".json";
-
-    return res.json({
-      jsonUrl
-    });
-
-  } catch(err) {
-
-    return res.status(500).json({
-      error: err.message
-    });
-  }
+  res.send("WORKING");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log("Server started");
 });

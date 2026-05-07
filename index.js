fd0a3067844b919fe6f8b023e35fa14f29e4b9ff
aprilholaaa <aprilholaaa@gmail.com>
2026-05-07 10:40:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,45 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("WORKING");
+  res.send("Reddit JSON API Running");
+});
+
+app.get("/convert", (req, res) => {
+
+  try {
+
+    let link = req.query.link;
+
+    if (!link) {
+      return res.status(400).json({
+        error: "No link provided"
+      });
+    }
+
+    // remove query params
+    link = link.split("?")[0];
+
+    // remove trailing slash
+    if (link.endsWith("/")) {
+      link = link.slice(0, -1);
+    }
+
+    const jsonUrl = link + ".json";
+
+    return res.json({
+      jsonUrl
+    });
+
+  } catch(err) {
+
+    return res.status(500).json({
+      error: err.message
+    });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server started");
+  console.log(`Server running on ${PORT}`);
 });
